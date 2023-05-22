b16c0de2f75a022a804f5e30f66ac2c0d00e0500
VarunKS <ksvarun001@gmail.com>
2023-05-22 20:43:07 +0530

    added back button and front button navigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 
 let num = 1;
 let timeout = 0;
+let totalFiles = 0;
 const defaultTimeGap = 5;
 
 function later(delay: number) {
@@ -20,36 +21,81 @@ function App() {
     fileSrc: '',
   });
 
+  function updateState(index: number) {
+    setState((state) => {
+      if (!state.files || !state.files[index]) {
+        return state;
+      }
+
+      return {
+        ...state,
+        fileSrc: URL.createObjectURL(state.files[index]),
+      };
+    });
+  }
+
   const callAgain = useCallback((i: number) => {
     let nextTimeGap = i;
     if (nextTimeGap === 0) {
       nextTimeGap = defaultTimeGap;
     }
     later(i).then(() => {
-      setState((state) => {
-        if (!state.files || !state.files[num]) {
-          return state;
-        }
-
-        return {
-          ...state,
-          fileSrc: URL.createObjectURL(state.files[num]),
-        };
-      });
+      updateState(num);
       num = num + 1;
       callAgain(nextTimeGap);
     });
   }, []);
 
   useLayoutEffect(() => {
+    let previousKeyPress: null | string = null;
     document.addEventListener('keydown', (e) => {
+      if (e.key === 'ArrowLeft' && num === 0) {
+        return;
+      }
+      if (e.key === 'ArrowRight' && num === totalFiles - 1) {
+        return;
+      }
       if (e.key === ' ' && timeout !== 0) {
         clearTimeout(timeout);
         timeout = 0;
+        previousKeyPress = e.key;
         return;
       }
       if (e.key === ' ' && timeout === 0) {
+        if (previousKeyPress !== ' ') {
+          num = num + 1;
+        }
         callAgain(0);
+        previousKeyPress = e.key;
+        return;
+      }
+      if (e.key === 'ArrowLeft' && timeout !== 0) {
+        clearTimeout(timeout);
+        timeout = 0;
+        const newNum = num - 2;
+        num = newNum;
+        if (newNum >= 0) {
+          updateState(newNum);
+        }
+        previousKeyPress = e.key;
+        return;
+      }
+      if (e.key === 'ArrowLeft' && timeout === 0) {
+        const newNum = num - 1;
+        num = newNum;
+        if (newNum >= 0) {
+          updateState(newNum);
+        }
+        previousKeyPress = e.key;
+        return;
+      }
+      if (e.key === 'ArrowRight') {
+        clearTimeout(timeout);
+        timeout = 0;
+        updateState(num);
+        num = num + 1;
+        previousKeyPress = e.key;
+        return;
       }
     });
   }, [callAgain]);
@@ -64,6 +110,7 @@ function App() {
               if (!e.target.files) {
                 return state;
               }
+              totalFiles = e.target.files.length;
               return {
                 ...state,
                 files: e.target.files,
